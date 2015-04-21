b8576a0c3e2986acd32921778ac53cdd3df7bad4
Mike <mikeknueppel@hotmail.com>
2015-04-20 17:03:17 -0700

    Add rails_12factor gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :quotes, only: [:show]  
+  resources :quotes, only: [:show]
+  root 'quotes#show' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
