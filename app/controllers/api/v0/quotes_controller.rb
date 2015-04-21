8f2c98990123d114b6579507f2e4dd2f03c0f2a0
Mike <mikeknueppel@hotmail.com>
2015-04-20 21:46:34 -0700

    Refactor QuotesController

--- app/controllers/api/v0/quotes_controller.rb
+++ app/controllers/api/v0/quotes_controller.rb
@@ -2,22 +2,28 @@ module Api
   module V0
     class QuotesController < ApplicationController
       respond_to :json
+      before_action :set_headers, only: :show
+      before_action :get_random_quote, only: :show
 
       def show
-        id = Random.rand(1..quotes.count)
-        quote = quotes[id - 1]
-        json = { body: quote.content }
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Last-Modified'] = (Time.now - 1.second).httpdate
-        render json
       end
 
       private
+
+      def get_random_quote
+        random_index = Random.rand(1..quotes.count) - 1
+        @quote = quotes[random_index]
+      end
 
       def quotes
         config = Rails.application.config
         config.quotes = Quote.all.to_a if config.quotes.empty?
         config.quotes
+      end
+
+      def set_headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Last-Modified'] = (Time.now - 1.second).httpdate
       end
     end
   end
---
+++ app/views/api/v0/quotes/show.jbuilder
@@ -0,0 +1,1 @@
+json.quote @quote.content
